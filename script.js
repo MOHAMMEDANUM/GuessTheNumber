cde7fbadd2b2ff6980579dd9e551a62e5415bf14
Mohammed Anum <160215305+MOHAMMEDANUM@users.noreply.github.com>
2025-05-24 18:26:01 +0530

    Added button for try again option

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let btn = document.querySelector("#btn")
                console.log(randomNo);
                console.log(attempts);
 
-              if(attempts>0){
+            // if(attempts>0){
 
                 
                 let userInput = prompt("Guess the Number Between 0 to 20")
@@ -23,27 +23,27 @@ let btn = document.querySelector("#btn")
                   outputMsg.style="background-color:green;border: none"
                   attempts = 3;
                   return;
-                  // outputMsg.style="border: none"
-                  
                 }
+
                 else if(userInput > randomNo){
                   outputMsg.textContent = `The Entered ${userInput} Number is Greater !!`
                   outputMsg.style="background-color:orange;border: none"
                 }
+
                 else if(userInput < randomNo){
                   outputMsg.textContent =  `The Entered ${userInput} Number is Lesser !!`
                   outputMsg.style="background-color:orange;border: none"
-                  
                 }
+
                 if(attempts > 0){
                   attempts--
                 }
+
                 else{
                   outputMsg.textContent =  `LOOSER LOOSER`
                   attempts = 3
                 }
-                attempted.textContent=` Attempts left is${attempts}`
-                }
-            }
 
+                attempted.textContent=` Attempts left is : ${attempts}`
+            }            
         btn.addEventListener("click" , generateNo);
