d9e5801853b3ecf4154a65a386164b5aa520fdcb
Mohammed Anum <160215305+MOHAMMEDANUM@users.noreply.github.com>
2025-05-24 21:07:04 +0530

    Added javascript code to display attempts left with images

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
         let startbtn = document.querySelector("#btn2")
         let outputGussed = document.querySelector("#winner")
         let outputMsg = document.querySelector("#msg")
-        let randomNo = Math.round(Math.random()*20);
+        let randomNo = Math.round(Math.random()*10);
         let attempted = document.querySelector("span")
+        let displayimg =document.querySelector(".images")
+        let imagesOfAttempts = document.getElementsByTagName("img")
         
         let attempts = 3;
 
@@ -54,7 +56,12 @@
 
                 if(attempts > 0){
                   attempts--
+                if(attempts==2){
+                    displayimg.style="display:flex"
+                    imagesOfAttempts[0].style="display:none"
+                    outputGussed.style="margin-left: 275px"
 
+                }
                   attempted.textContent=` Attempts left is : ${attempts}`
                 }
 
@@ -65,8 +72,8 @@
                   startbtn.style="display:flex;justify-content: center;align-items: center;"
                   generateBtn.style="display:none"
                   outputMsg.style="background-color:red"
-                  attempted.textContent="Try again no attempts left"
-                  outputGussed.textContent = randomNo;
+                  attempted.textContent=`Correct Number was ${randomNo}`
+                  outputGussed.textContent = randomNo
 
 
                   return
